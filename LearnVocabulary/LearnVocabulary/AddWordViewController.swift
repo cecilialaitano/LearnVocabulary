c16ad8256f60d95d4ceaeb2799d69d8e112f1edf
LAITC002 <cecilia.x.laitano.-nd@disney.com>
2018-05-01 16:28:01 -0700

    Add placeholder UI: Add word & empty list

---
+++ LearnVocabulary/LearnVocabulary/AddWordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddWordViewController.swift
+//  LearnVocabulary
+//
+//  Created by Cecilia Laitano on 01/05/2018.
+//  Copyright © 2018 panadie. All rights reserved.
+//
+
+import UIKit
+
+class AddWordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

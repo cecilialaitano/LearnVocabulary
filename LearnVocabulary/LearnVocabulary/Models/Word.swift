4a0619445b8600b3f2db62167be1eae6f1201483
LAITC002 <cecilia.x.laitano.-nd@disney.com>
2018-05-02 10:01:02 -0700

    ADD: Word model

---
+++ LearnVocabulary/LearnVocabulary/Models/Word.swift
@@ -0,0 +1,16 @@
+//
+//  Word.swift
+//  LearnVocabulary
+//
+//  Created by Cecilia Laitano on 01/05/2018.
+//  Copyright © 2018 panadie. All rights reserved.
+//
+
+import Foundation
+
+struct Word {
+  var word: String
+  var definition: String
+  var example: String?
+  var dateAdded: Date
+}
